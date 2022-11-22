1965983faefd1f7c7f08317c2d54e88cd3654792
Kelvin Baiden <denlinato@gmail.com>
2022-11-22 07:48:11 +0000

    integrating spotify api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import { useState, Fragment } from "react";
 import useGoogleToken from "./hooks/usegoogleToken";
 import InputComponent from "./components/InputComponent";
 import Main from "./components/Main";
-
+import axios from 'axios';
+import querystring from 'query-string'
 export default function App() {
 
 
+ async function spotify (){
+
+  const res = 'https://accounts.spotify.com/authorize?'+ querystring.stringify({
+    response_type: 'token',
+   client_id: process.env.REACT_APP_SPOTIFYClIENTID,
+  redirect_uri:process.env.REACT_APP_REDIRECTURL,
  
+  })
+
+ window.location =  res;
+
+
+
+ }
 
 
   
@@ -15,6 +29,7 @@ export default function App() {
   //  setS(client);
   return (
     <Fragment>
+    <button onClick={spotify}>spotify</button>
       <InputComponent/>
       <Main/>
     </Fragment>
