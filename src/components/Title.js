51d738a615bc907ffa8781a6ace28b774272e0d1
kelvin <kelvin950>
2023-01-04 23:09:10 +0000

    changing the title

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,14 +2,15 @@ export default function Title() {
   return (
     <div className="mt-6  mb-40">
       <h1 className="font-extrabold text-4xl sm:text-5xl lg:text-7xl text-center">
-     
         Translate
       </h1>
       <p className=" mt-6 mb-6  leading-5 p-10 pt-4 pb-4 leading-loose text-zinc-400  w-full md:w-21 text-center md:border-solid md:order-2 border-blue-500 rounded-lg  des">
-        A simple and fast application that converts that youtube playlist which
-        you cannot always play because your phone sleeps or you switch to
-        twitter to retweet a tweet or instagram to stalk your crush. Nobody
-        wants to pay for youtube premium anyways so why dont you just try this.
+        A straightforward and quick program that makes that YouTube playlist,
+        You are unable to play every time your phone goes to sleep, you switch
+        to Instagram to follow your crush, or you go to Twitter to retweet
+        something. You also save time by not having to manually add songs and
+        make the playlist. Why don't you try this instead since nobody wants to
+        pay for YouTube Premium?
       </p>
     </div>
   );
